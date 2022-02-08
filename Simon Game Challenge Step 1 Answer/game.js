e56c6d22bcb48b45cf82e17091ad3c3396839c6d
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-02-08 15:11:51 +0100

    Add files via upload

---
+++ Simon Game Challenge Step 1 Answer/game.js
@@ -0,0 +1,11 @@
+var buttonColours = ["red", "blue", "green", "yellow"]
+
+var gamePattern = []
+
+function nextSequence() {
+    
+    let randomNumber = Math.floor(Math.random() * 4)
+    var randomChosenColour = buttonColours[randomNumber]
+
+}
+
